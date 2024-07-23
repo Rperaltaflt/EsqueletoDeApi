db901c22e20a10490f45ce357f4e6f43e8a94c5a
rperalta <rperalta@COMPUTOSRP>
2024-07-23 12:33:02 -0300

    Agregar archivos de proyecto.

---
+++ EsqueletoDeApi.RepositorioBD/Contratos/IGenericoRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsqueletoDeApi.RepositorioBD.Contratos
+{
+    public interface IGenericoRepositorio<unModelo> where unModelo : class //Un modelo va a ser una clase :class
+    {
+        //Esto va a retornar un Usuario, un Producto o lo que sea, colocando un filtro si se quiere o si no 
+        //esto es como poner SELECT * FROM USUARIOS WHERE ROL = "ALGO"
+        IQueryable<unModelo> Consultar(Expression<Func<unModelo, bool>>? filtro = null);
+        Task<unModelo> Crear(unModelo modelo);
+        Task<bool> Editar(unModelo modelo);
+        Task<bool> Eliminar(unModelo modelo);
+    }
+}
